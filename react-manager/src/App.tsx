374b2c12a6392e0349aa0b59cfa41d96ec40f1af
BensonWang0512 <pingchihwang512@gmail.com>
2024-01-05 15:51:18 -0800

    React Hook - memo & useMemo & useCallback
    This is the record of memo / useMemo / useCallback

--- react-manager/src/App.tsx
+++ react-manager/src/App.tsx
@@ -1,30 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { memo, useCallback, useMemo, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const total1 = () => {
+    // eslint-disable-next-line no-console
+    console.log('total1...')
+    const list = [1, 3, 5, 7, 9]
+    return list.reduce((prev, current) => prev + current, 0)
+  }
+  const total2 = useMemo(() => {
+    // eslint-disable-next-line no-console
+    console.log('total2...')
+    const list = [1, 3, 5, 7, 9]
+    return list.reduce((prev, current) => prev + current, 0)
+  }, [])
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  const handleChildClick = useCallback(() => {
+    // eslint-disable-next-line no-console
+    console.log('子节点点击')
+  }, [])
   return (
     <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+      <p>欢迎学习React后台课程</p>
+      <p>
+        <span>Count值：{count}</span>
+        <button onClick={handleClick}>按钮</button>
+      </p>
+      <p>{total1()}</p>
+      <p>{total2}</p>
+      <Child onClick={handleChildClick} />
     </div>
   )
 }
+
+// eslint-disable-next-line react/display-name
+const Child = memo(({ onClick }: any) => {
+  console.log('child...')
+  return (
+    <p>
+      我是子节点<button onClick={onClick}>子节点按钮</button>
+    </p>
+  )
+})
 
 export default App
